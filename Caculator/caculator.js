3ad42f77589c0f230407d3131c495bba4dac12a3
duyhelloworld <codedaovoiduy@gmail.com>
2022-12-04 23:53:43 +0700

    modified:   Caculator/caculator.js

--- Caculator/caculator.js
+++ Caculator/caculator.js
@@ -1,46 +1,6 @@
-// const multiOpe = document.getElementById("multiOpe");
-// const minusOpe = document.getElementById("minusOpe");
-// const divideOpe = document.getElementById("divideOpe");
-// const plusOpe = document.getElementById("plusOpe");
-// const equalOpe = document.getElementById("equalOpe");
-// const percentOpe = document.getElementById("percentOpe");
-// const dotBtn = document.getElementById("dotBtn");
-// const acBtn = document.getElementById("acBtn");
-// const resultBox = document.querySelector(".result-box");
-// resultBox.innerHTML = "";
-// const operators = [plusOpe, minusOpe, multiOpe, divideOpe, percentOpe];
-// let runningTotal = 0;
-
-// let number = "";
-// let operatorClicked = false;
-// let key = 0;
-// function handleNumber(element) {
-//   let pressValue = element.innerText;
-//     number += pressValue;
-//     key++;
-//   resultBox.innerHTML =
-//     "<h1 style='color: black'>" + number  + "</h1>";
-//     if (clickOperator(number)) {
-//         localStorage.setItem(key, number);
-//   }
-// }
-
-// function clickOperator(number) {
-//   operators.forEach((operator) => {
-//     operator.onclick = () => {
-//       operatorClicked = true;
-//     };
-//       if (operatorClicked) {
-//           let nextNum = localStorage.getItem(key);
-//           console.log(nextNum);
-//           return true;
-//       }
-//       return false;
-//   });
-// }
-
-// function clickToCal() {}
-
+setTimeout(() => {
+  alert("You can type code by your keyboard")
+}, 2000);
 
 let resultScreen = document.getElementById("resultScreen");
 
